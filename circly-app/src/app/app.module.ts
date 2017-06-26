69ab828f22d9ccf75b9abaa19b7a95e473af48ff
Francesco <pongiof@gmail.com>
2017-06-26 21:49:31 +0100

    NodeJS configuration

--- circly-app/src/app/app.module.ts
+++ circly-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     MaterialModule,
   ],
