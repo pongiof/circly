e943e964c23eb31b971d19cf9074b06c1842503e
Francesco <pongiof@gmail.com>
2017-06-27 14:31:40 +0100

    Service worker support using sw-cache. Hosting material fonts locally to support sw-cache.

--- circly-app/src/main.ts
+++ circly-app/src/main.ts
@@ -8,4 +8,13 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => console.log(
+            'SW registration successful with scope: ', registration.scope));
+    }
+  })
+  .catch(err => console.error(err));
