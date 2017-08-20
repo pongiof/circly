74e42e62ff2219b74e071215f681eb5451a10354
Francesco <pongiof@gmail.com>
2017-08-20 22:14:36 +0100

    Fix micros to string in backend api

--- backend/api/controllers/GetAllCollections.js
+++ backend/api/controllers/GetAllCollections.js
@@ -10,13 +10,13 @@ var protos = require("../../protos/out/ts/model.js")
 function GetAllCollections(req, res) {
     var mock1  = protos.model.Collection.create()
     mock1.name = "mock collection"
-    mock1.createdMicros = 123456
+    mock1.createdMicros = "123456"
     mock1.description = "simple mock collection"
     mock1.subscriberId = []
 
     var mock2  = protos.model.Collection.create()
     mock2.name = "mock collection 2"
-    mock2.createdMicros = 123456
+    mock2.createdMicros = "123456"
     mock2.description = "simple mock collection 2"
     mock2.subscriberId = []
 
