f714cf62feefd23572a1332419f5c490839971bc
Francesco <pongiof@gmail.com>
2017-08-20 22:24:05 +0100

    log console headers in backend addcollection()

--- backend/api/controllers/AddCollection.js
+++ backend/api/controllers/AddCollection.js
@@ -8,7 +8,12 @@ var protobuf = require("protobufjs");
 var protos = require("../../protos/out/ts/model.js")
 
 function AddCollection(req, res) {
-    console.log(req.body);
+    console.log("got req headers:");
+    console.log(req.headers);
+    var hrTime = process.hrtime();
+    var currentMicros = Math.round(hrTime[0] * 1000000 + hrTime[1] / 1000);
     var collection = protos.model.Collection.fromObject(req.body);
-    res.json(collection)
+    collection.createdMicros = currentMicros.toString();
+
+    res.send(collection.toJSON());
 }
