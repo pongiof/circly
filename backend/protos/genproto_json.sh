d5528c7ecb4d36e8e66c0a3007dc971a1ffff71c
Francesco <pongiof@gmail.com>
2017-08-21 21:48:58 +0100

    New script for managing APIs

--- backend/protos/genproto_json.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ./out/json/
-
-protobuf-jsonschema --format yaml model.proto > out/json/model.yaml
--- backend/protos/update_protos.sh
+++ backend/protos/update_protos.sh
@@ -4,3 +4,12 @@ mkdir -p ./out/ts/
 ../node_modules/protobufjs/cli/bin/pbts -o out/ts/model.d.ts out/ts/model.js
 
 cp -r out/ts/* ../../circly-app/src/app/shared/protos
+
+function updateApi {
+    sed '/definitions\:/,$d' $1 > tmp
+    protobuf-jsonschema --format yaml model.proto >> tmp
+    mv tmp $1
+}
+
+updateApi ../api/swagger/swagger.yaml
+updateApi ../openapi.yaml
